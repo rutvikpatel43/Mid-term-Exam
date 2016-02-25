6dce9c153952d2061d19ff6de7c50618f57cebbd
rutvikpatel43 <rutvikpatel43@gmail.com>
2016-02-25 15:04:57 -0500

    correted file commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Favorite People' });
 });
 /* GET People page. */
 router.get('/people', function(req, res, next) {
